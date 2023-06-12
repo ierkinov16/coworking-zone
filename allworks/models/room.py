1639d3099112a31d8d2d2a4587389841d194e9ea
Saidahror13 <01erkhinov@gmail.com>
2023-06-12 17:14:46 +0500

    [70  / 100]

--- allworks/models/room.py
+++ allworks/models/room.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Room(models.Model):
     class RoomType(models.TextChoices):
-        focus = 'focus', 'Focus'
-        team = 'team', 'Team'
-        conference = 'conference', 'Conference',
+        FOCUS = 'focus', 'Focus'
+        TEAM = 'team', 'Team'
+        CONFERENCE = 'conference', 'Conference',
 
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=20, choices=RoomType.choices, default=RoomType.team)
